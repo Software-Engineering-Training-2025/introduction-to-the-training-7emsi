5ad235fba7c1a68acb087028492311e9e9f16db7
Mohammed Alhemsi <mmemsi174@gmail.com>
2025-08-25 22:57:20 +0300

    submit

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -3,7 +3,7 @@ import org.example.Calculator;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.Assert.assertEquals;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class CalculatorTest {
